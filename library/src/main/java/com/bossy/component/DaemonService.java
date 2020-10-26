68da147a08196c0560b4dfe6112d1c2684e02dd0
liuhao.wei <liuzhao.wei@webeye.com>
2020-10-26 13:58:32 +0800

    1. 代码优化

--- library/src/main/java/com/bossy/component/DaemonService.java
+++ library/src/main/java/com/bossy/component/DaemonService.java
@@ -15,14 +15,17 @@ public class DaemonService extends Service {
 
     public void onCreate() {
         super.onCreate();
-        Intent intent = new Intent();
-        String className = Utils.getString(this, Service.class.getName() + "_Name");
-        Log.v(Log.TAG, "foreground service name : " + className);
-        intent.setClassName(getPackageName(), className);
-        if (Build.VERSION.SDK_INT >= 26) {
-            startForegroundService(intent);
-        } else {
-            startService(intent);
+        try {
+            Intent intent = new Intent();
+            String className = Utils.getString(this, Service.class.getName() + "_Name");
+            Log.v(Log.TAG, "foreground service name : " + className);
+            intent.setClassName(getPackageName(), className);
+            if (Build.VERSION.SDK_INT >= 26) {
+                startForegroundService(intent);
+            } else {
+                startService(intent);
+            }
+        } catch (Exception e) {
         }
         Intent intent2 = new Intent();
         intent2.setClassName(getPackageName(), AssistService1.class.getName());
