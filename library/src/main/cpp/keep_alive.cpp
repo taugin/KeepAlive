2159bdaa9dd3a8e18c8aedcd98fa27ceb805ab46
liuhao.wei <liuzhao.wei@webeye.com>
2020-10-28 14:15:35 +0800

    1. 路径优化

--- library/src/main/cpp/keep_alive.cpp
+++ library/src/main/cpp/keep_alive.cpp
@@ -56,7 +56,7 @@ native_lockFile(JNIEnv *env, jclass jobj,
     lock_file(lock_file_path);
 }
 
-#define JNIREG_CLASS "com.bossy.component.DaemonMain"//指定要注册的类
+#define JNIREG_CLASS "com/bossy/component/DaemonMain"//指定要注册的类
 
 static jstring findJniRegClass(JNIEnv *env) {
     jclass clazz = env->FindClass("java/lang/System");
