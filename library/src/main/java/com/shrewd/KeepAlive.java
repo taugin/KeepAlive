dc37aa62bdabbd1264ee2ae696bc53b61443ecef
liuhao.wei <liuzhao.wei@webeye.com>
2020-10-29 10:24:26 +0800

    1. 增加startService接口

--- library/src/main/java/com/shrewd/KeepAlive.java
+++ library/src/main/java/com/shrewd/KeepAlive.java
@@ -3,11 +3,13 @@ package com.shrewd;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.os.Build;
 
 import com.shrewd.daemon.JavaDaemon;
 import com.shrewd.cmp.DaemonInstrumentation;
 import com.shrewd.cmp.DaemonReceiver;
 import com.shrewd.cmp.DaemonService;
+import com.shrewd.log.Log;
 import com.shrewd.utils.Utils;
 
 public class KeepAlive {
@@ -17,5 +19,18 @@ public class KeepAlive {
         }
         JavaDaemon.getInstance().init(context, new Intent(context, DaemonService.class), new Intent(context, DaemonReceiver.class), new Intent(context, DaemonInstrumentation.class));
         JavaDaemon.getInstance().startAppLock(context, new String[]{"daemon", "assist1", "assist2"});
+    }
+
+    public static void startService(Context context, Class<?> service) {
+        try {
+            Intent serviceIntent = new Intent(context, service);
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                context.startForegroundService(serviceIntent);
+            } else {
+                context.startService(serviceIntent);
+            }
+        } catch (Exception e) {
+            Log.e(Log.TAG, "error : " + e);
+        }
     }
 }
